460055a688ffb4fdadcdabf0c6d8677ec8e7d29c
Sima Fayad <simafayad.1@gmail.com>
2023-03-11 22:50:51 -0600

    Calculator Lab

---
+++ index.js
@@ -0,0 +1,63 @@
+function add() {
+    // we'll fill this in shortly
+  }
+  function subtract() {
+    // we'll fill this in  shortly
+  }
+  function multiply() {
+    // we'll fill this in shortly
+  }
+  function divide() {
+    // we'll fill this in shortly
+  }
+  function add() {
+    return "something"
+  }
+  function add(a, b) {
+    return "something";
+  }
+  function add(a, b) {
+    return a + b;
+  }
+  function subtract(a, b) {
+    return a - b;
+  }
+  function multiply(a, b) {
+    return a * b;
+  }
+  function divide(a, b) {
+    return a / b;
+  }
+  function increment(a) {
+    return a +1
+  }
+  function decrement(a) {
+    return a - 1
+  }
+
+
+  function makeInt (a) { 
+    number = parseInt(a,10)
+    return number
+  }
+  
+  function makeInr(n,base) {
+    return 0
+  }
+
+
+  function makeInt(NaN){
+    return 0
+  }
+  function makeInt(string) {
+    return parseInt(string,10)
+  }
+
+  function preserveDecimal(string){
+    return parseFloat(string,10)
+  }
+   
+  function makeFloat(NaN){
+    return 0
+  }
+  
